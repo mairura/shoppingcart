c07fc20cc6270b24cda667c6b93d13e57643df6f
unknown <mairurated@gmail.com>
2022-03-30 08:50:19 +0300

    Some modification

--- js/main.js
+++ js/main.js
@@ -1,3 +1,34 @@
+// Cart
 let cartIcon = document.querySelector("#cart-icon");
 let cart = document.querySelector(".cart");
 let closeCart = document.querySelector("#close-cart");
+
+cartIcon.onclick = () => {
+  cart.classList.add("active");
+};
+closeCart.onclick = () => {
+  cart.classList.remove("active");
+};
+
+// Cart Working
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", ready);
+} else {
+  ready();
+}
+
+//Making function
+function ready() {
+  var removeCartButtons = document.getElementsByClassName("cart-remove");
+  console.log(removeCartButtons);
+  for (var i = 0; i < removeCartButtons.length; i++) {
+    var button = removeCartButtons.length[i];
+    button.addEventListener("click", removeCartItem);
+  }
+}
+
+//Remove Item from cart
+function removeCartItem(event) {
+  var buttonClicked = event.target;
+  buttonClicked.parentElement.remove();
+}
